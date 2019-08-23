02729295d566a671c0e0e16ebb4ae930a12d6888
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-23 15:06:13 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,8 +13,12 @@ class FiguresController < ApplicationController
   post '/figures' do
     #binding.pry
     @figure = Figure.create(params[:figure])
-    @figure.titles = Title.find_or_create_by(:name => params[:title][:name])
-    @figure.landmarks = Landmark.find_or_create_by(:name => params[:landmark][:name])
+    if params[:title][:name]
+      @figure.titles = Title.find_or_create_by(:name => params[:title][:name])
+    end
+    if params[:landmark][:name]
+      @figure.landmarks = Landmark.find_or_create_by(:name => params[:landmark][:name])
+    end
     @figure.save
     redirect "/figures/#{@figure.id}"
   end
